b3bb041e560ea027c73c25e345c9cca82e439c2e
AshiChoudhary12 <79406861+AshiChoudhary12@users.noreply.github.com>
2021-09-24 17:07:51 +0530

    Add files via upload

---
+++ GreatDealsCommon/GreatDealsBackend/src/main/java/com/gd/admin/GreatDealsBackendApplication.java
@@ -0,0 +1,16 @@
+package com.gd.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+@EntityScan({"com.gd.common.entity", "com.gd.admin.user"})
+public class GreatDealsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreatDealsBackendApplication.class, args);
+	}
+
+}
+	
---
+++ GreatDealsCommon/GreatDealsBackend/src/main/java/com/gd/admin/user/UserRepository.java
@@ -0,0 +1,24 @@
+package com.gd.admin.user;
+
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.gd.common.entity.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+	@Query()
+	public User getUserByEmail(@Param("email") String email);
+
+	public Long countById(Integer id);
+	@Query("UPDATE User SET enabled = ?2 Where id = ?1")
+	@Modifying
+	public void updateEnableStatus(Integer id, boolean enabled);
+}
